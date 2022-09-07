02166bf9e013433562a1398ecef60218c8de466c
nehru35 <sonehru@gmail.com>
2022-09-07 23:23:46 +0100

    Adicionado o bot

--- sistemaDeLeilao/includes/sidebar.php
+++ sistemaDeLeilao/includes/sidebar.php
@@ -1,9 +1,4 @@
 <div class="profile_nav">
-  <ul>
-    <li><a href="profile.php">Perfil</a></li>
-    <li><a href="update_password.php">Mudar senha</a></li>
-    <li><a href="my_booking.php">Minhas ofertas</a></li>
-    <li><a href="logout.php">Sair</a></li>
-  </ul>
+  
 </div>
 </div>
